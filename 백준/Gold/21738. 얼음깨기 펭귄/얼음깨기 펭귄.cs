98875284ee20c8f88d0a63679c559d3cc7339122
Ragura635 <ragura635@gmail.com>
2025-05-18 19:14:49 +0900

    [Gold V] Title: 얼음깨기 펭귄, Time: 432 ms, Memory: 55328 KB -BaekjoonHub

--- 백준/Gold/21738. 얼음깨기 펭귄/얼음깨기 펭귄.cs
+++ 백준/Gold/21738. 얼음깨기 펭귄/얼음깨기 펭귄.cs
@@ -1,74 +1,77 @@
 class Program
 {
     static List<int>[] node;
-    static bool[] visited;
-    static int depth;
-    static int[] minDepth;
-    static int penguinBlock;
+    static int[] dist;
+    static int blocks, support, penguinBlock;
     
     static void Main(string[] args)
-    {
-        //1 https://chatgpt.com/c/68299ad5-de6c-8003-a7d1-67932a234672
+    {        
+        //1 https://www.acmicpc.net/problem/21738 BFS버전
         var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-        string[] inputs;
-        
-        inputs = sr.ReadLine().Split(' ');
-        int blocks = int.Parse(inputs[0]);
+
+        string[] inputs = sr.ReadLine().Split();
+        blocks = int.Parse(inputs[0]);
         node = new List<int>[blocks + 1];
         for (int i = 0; i <= blocks; i++)
         {
             node[i] = new List<int>();
         }
-        visited = new bool[blocks + 1];
-        
-        int roots = int.Parse(inputs[1]);
-        minDepth = new int[roots];
-        
+        support =  int.Parse(inputs[1]);
         penguinBlock = int.Parse(inputs[2]);
         
         for (int i = 0; i < blocks - 1; i++)
         {
-            inputs = sr.ReadLine().Split(' ');
+            inputs = sr.ReadLine().Split();
             int a = int.Parse(inputs[0]);
             int b = int.Parse(inputs[1]);
-        
             node[a].Add(b);
             node[b].Add(a);
         }
-
-        for (int i = 0; i < roots; i++)
-        {
-            Array.Fill(visited, false);
-            depth = 0;
-            DFS(i + 1, 0);
-            minDepth[i] = depth;
-        }
-        
-        Array.Sort(minDepth);
-        
-        sw.WriteLine(blocks - minDepth[0] - minDepth[1] -1);
+            
+        BFS_EarlyExit(penguinBlock, out int d1, out int d2);
+        int result = blocks - d1 - d2 - 1;
+        sw.WriteLine(result);
         sw.Flush();
     }
-    
-    static bool DFS(int now, int nowDepth)
+
+    static void BFS_EarlyExit(int start, out int dist1, out int dist2)
     {
-        visited[now] = true;
+        Queue<int> q = new Queue<int>();
+        dist = new int[blocks + 1];
+        Array.Fill(dist, -1);
 
-        if (now == penguinBlock)
-        {
-            depth = nowDepth;
-            return true;
-        }
+        q.Enqueue(start);
+        dist[start] = 0;
 
-        foreach (int next in node[now])
+        dist1 = dist2 = 0;
+        List<int> found = new List<int>();
+
+        while (q.Count > 0)
         {
-            if (!visited[next])
+            int now = q.Dequeue();
+
+            // ✅ now가 지지대면 그때 거리 저장
+            if (now <= support)
             {
-                if (DFS(next, nowDepth + 1)) return true;
+                found.Add(dist[now]);
+                if (found.Count == 2)
+                {
+                    found.Sort();
+                    dist1 = found[0];
+                    dist2 = found[1];
+                    return;
+                }
+            }
+
+            foreach (int next in node[now])
+            {
+                if (dist[next] == -1)
+                {
+                    dist[next] = dist[now] + 1;
+                    q.Enqueue(next);
+                }
             }
         }
-
-        return false;
     }
 }
